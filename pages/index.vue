f02a4389a4c618fff432410e471420b3862195e1
Foster Douglas <foster@sandcastle.co>
2023-09-10 02:08:20 -0700

    Initial copy

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="mt-5">
+    <div id="app" class="container-fluid py-5">
+      <h1 class="text-center">The Chameleon and</h1>
+      <h1 class="text-center">The Rainbow-Shell Turtle</h1>
+      <hr class="w-50 mx-auto my-4" />
+      <p class="text-center">Coming soon!</p>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+//
+</script>
+
+<style lang="scss">
+h1 {
+  font-family: vag-rundschrift-d, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+p {
+  font-family: gelica, sans-serif;
+  font-weight: 300;
+  font-style: normal;
+}
+</style>
